82f8039641970371adec0a5b210ad636acda802f
a-todd834 <55078036+a-todd834@users.noreply.github.com>
2021-10-07 12:09:10 -0600

    update

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -507,12 +507,35 @@ export const Home = () => {
           </div>
         </div>
       </section>
+      <section className='p-5 bg-dark '>
+        <div className='row py-5'>
+          <img
+            className='img-fluid col-lg-6'
+            src='./undraw_delivery_address_03n0.svg'
+            alt='contact me by email'
+          ></img>
+          <div className='col-lg-6 pt-3 text-center'>
+            <h2 className='text-light'>Contact Info</h2>
+            <ul className='list-group list-group-flush lead text-start p-lg-5'>
+              <li className='list-group-item bg-dark text-light'>
+                <span className='fw-bold'>Location:</span> Cochrane, Alberta,
+                Canada
+              </li>
+              <li className='list-group-item bg-dark text-light'>
+                <span className='fw-bold'>Phone:</span> 403-542-6637
+              </li>
+              <li className='list-group-item bg-dark text-light'>
+                <span className='fw-bold'>Email:</span> todd.r.adrian@gmail.com
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
       <footer className='p-5 bg-dark text-light text-center position-relative'>
         <div className='container'>
           <h6 small className='fw-light'>
             Copyright &copy; 2021 Adrian Todd
           </h6>
-
           <a href='#' className='position-absolute bottom-0 end-0 p-5'>
             <ArrowUpCircle className='h1 text-secondary'></ArrowUpCircle>
           </a>
